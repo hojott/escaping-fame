84eebd69a6568f7a08637666d514d63dd3d566cb
Luukas Lehtinen <lukituubi@gmail.com>
2023-05-12 20:56:41 +0300

    Add main loop

--- src/game.py
+++ src/game.py
@@ -5,13 +5,29 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen: tuple = pygame.display.set_mode(SCREEN_SIZE)
+        self.state = "menu"
 
         self.start_game()
     
     def start_game(self):
-        self.running: bool = True'
+        self.running: bool = True
+
+        clock = pygame.time.Clock()
         
         while self.running:
-            continue
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if self.state == "menu":
+                        if event.key == pygame.K_q:
+                            self.running = False
+
+            pygame.display.flip()
+
+            clock.tick(60)
+
+        pygame.quit()
+
 
 game = Game()
