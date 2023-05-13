19619112eb18029a730f7f56121a9b7183bf665d
Sakari Marttinen <sakari.marttinen@paivola.fi>
2023-05-13 13:26:58 +0300

    Fix diagonal movement

--- src/logic/player.py
+++ src/logic/player.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 
 from ..constants import *
 from ..graphics import drawPlayer
@@ -21,8 +22,21 @@ class Player:
         drawPlayer(self.screen, self.pos_on_screen)
     
     def move(self) -> None:
-        self.pos_on_screen[0] += self.velocity[0]
-        self.pos_on_screen[1] += self.velocity[1]
+        movement = [float, float]
+
+        if self.velocity[0] == 0 or self.velocity[1] == 0:
+            movement[0] = self.velocity[0]
+            movement[1] = self.velocity[1]
+        
+        else:
+            # Fix diagonal movement being op
+            velocity_pyth = math.sqrt(self.velocity[0]**2 + self.velocity[1]**2)
+
+            movement[0] = self.velocity[0]*abs(self.velocity[0])/velocity_pyth
+            movement[1] = self.velocity[1]*abs(self.velocity[1])/velocity_pyth
+
+        self.pos_on_screen[0] += movement[0]
+        self.pos_on_screen[1] += movement[1]
 
     def input(self) -> None:
         keys = pygame.key.get_pressed()
@@ -30,28 +44,28 @@ class Player:
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             # Positive x is east
             if self.velocity[0] < MAX_VELOCITY:
-                self.velocity[0] +=1
+                self.velocity[0] += 1
             if self.velocity[0] == MAX_VELOCITY:
                 self.velocity[0] = MAX_VELOCITY
 
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             # Negative x is west
             if self.velocity[0] > -MAX_VELOCITY:
-                self.velocity[0] -=1
+                self.velocity[0] -= 1
             if self.velocity[0] == -MAX_VELOCITY:
                 self.velocity[0] = -MAX_VELOCITY
 
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             # Negative y is north
             if self.velocity[1] > -MAX_VELOCITY:
-                self.velocity[1] -=1
+                self.velocity[1] -= 1
             if self.velocity[1] == -MAX_VELOCITY:
                 self.velocity[1] = -MAX_VELOCITY
 
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             # Positive y is west
             if self.velocity[1] < MAX_VELOCITY:
-                self.velocity[1] +=1
+                self.velocity[1] += 1
             if self.velocity[1] == MAX_VELOCITY:
                 self.velocity[1] = MAX_VELOCITY
 
@@ -64,7 +78,7 @@ class Player:
                     self.velocity[0] -= 1
                 else:
                     self.velocity[0] = 0
-                    
+
             elif self.velocity[0] < 0:
                 if self.velocity[0] < 1:
                     self.velocity[0] += 1
