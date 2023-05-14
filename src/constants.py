7737fc0393a25335e3343257ddf4a13af2a51115
Sakari Marttinen <sakari.marttinen@paivola.fi>
2023-05-14 12:25:19 +0300

    Fix SIZE (maybe)

--- src/constants.py
+++ src/constants.py
@@ -11,5 +11,5 @@ FONT_SIZE: int = 25
 
 PLAYER_SIZE: tuple = (50, 55)
 HUMAN_SIZE: tuple = (50, 50)
-FAN_SIZE: tuple = (600, 600)
+FAN_SIZE: tuple = (90, 90)
 CAR_SIZE: tuple = (100, 50)
--- src/logic/player.py
+++ src/logic/player.py
@@ -67,7 +67,7 @@ class Player:
                 self.game.world.position[0] += movement[0]
                 for human in self.game.world.humans:
                     human[1].pos_on_screen[0] -= movement[0]
-                    human[1].bounding_box.update((human[1].pos_on_screen[0]-self.velocity[0], human[1].pos_on_screen[1]), HUMAN_SIZE)
+                    human[1].bounding_box.update((human[1].pos_on_screen[0]-self.velocity[0], human[1].pos_on_screen[1]), human[1].SIZE)
             
         if movement[0] <= 0:
             if self.game.world.position[0] <= 0 or self.pos_on_screen[0] > 475:
@@ -78,7 +78,7 @@ class Player:
                 self.game.world.position[0] += movement[0]
                 for human in self.game.world.humans:
                     human[1].pos_on_screen[0] -= movement[0]
-                    human[1].bounding_box.update((human[1].pos_on_screen[0]-self.velocity[0], human[1].pos_on_screen[1]), HUMAN_SIZE)
+                    human[1].bounding_box.update((human[1].pos_on_screen[0]-self.velocity[0], human[1].pos_on_screen[1]), human[1].SIZE)
 
         # Vertical movement
         if movement[1] >= 0:
@@ -90,7 +90,7 @@ class Player:
                 self.game.world.position[1] += movement[1]
                 for human in self.game.world.humans:
                     human[1].pos_on_screen[1] -= movement[1]
-                    human[1].bounding_box.update((human[1].pos_on_screen[0], human[1].pos_on_screen[1]-self.velocity[1]), HUMAN_SIZE)
+                    human[1].bounding_box.update((human[1].pos_on_screen[0], human[1].pos_on_screen[1]-self.velocity[1]), human[1].SIZE)
         
         if movement[1] <= 0:
             if self.game.world.position[1] <= 0 or self.pos_on_screen[1] > 475:
@@ -101,7 +101,7 @@ class Player:
                 self.game.world.position[1] += movement[1]
                 for human in self.game.world.humans:
                     human[1].pos_on_screen[1] -= movement[1]
-                    human[1].bounding_box.update((human[1].pos_on_screen[0], human[1].pos_on_screen[1]-self.velocity[1]), HUMAN_SIZE)
+                    human[1].bounding_box.update((human[1].pos_on_screen[0], human[1].pos_on_screen[1]-self.velocity[1]), human[1].SIZE)
 
     def input(self) -> None:
         keys = pygame.key.get_pressed()
