9ad887279a57225dca5c4e376391c8a6876f5c5a
Sakari Marttinen <sakari.marttinen@paivola.fi>
2023-05-14 11:54:07 +0300

    Update map

--- src/map.py
+++ src/map.py
@@ -1,7 +1,8 @@
 maps = [
     [ #0
-        [20000, 800], # TILE_SIZE = (400, 400)
-        ['sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2'],
-        ['sw8','sw8','sw9','sw7','sw8','sw9','sw9','sw8','sw7','sw9','sw8','sw9','sw8','sw9','sw8','sw7','sw8','sw9','sw9','sw8','sw7','sw9','sw8','sw9','sw8','sw9','sw7','sw8','sw9','sw8','sw7','sw9','sw8','sw7','sw8','sw9','sw9','sw8','sw7','sw7','sw8','sw9','sw8','sw8','sw7','sw8','sw9','sw9','sw7','sw7','sw8','sw9','sw8','sw7','sw9','sw9','sw8','sw8','sw9','sw7'],
+        [10000, 800], # TILE_SIZE = (400, 400)
+        ['sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2','sw2','sw4','sw3','sw5','sw2','sw4','sw3','sw5','sw1','sw2'],
+        ['sw8','sw8','sw9','sw7','sw8','sw9','sw9','sw8','sw7','sw9','sw8','sw9','sw8','sw9','sw8','sw7','sw8','sw9','sw9','sw8','sw7','sw9','sw8','sw9','sw8'],
+        ['b3','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b1','b2','b3','b1','b1']
     ]
 ]
